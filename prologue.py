f42ad4c1f3b005503af8fee8d616853c31bbb324
moriko <126028579+morikoaraiguma@users.noreply.github.com>
2023-02-22 07:49:57 +0000

    finish1

--- prologue.py
+++ prologue.py
@@ -1,19 +1,19 @@
 ##
-#p.oscrunner
-#little fun thing personal project little goofy thing!
-#2/15/22 - 2/17/22
+#prologue to my little object show sim
+#2/15/23 - 2/22/23
 lazy = 0
 name = input("hello! whta is your name? ")
 object = input("and whta is your object? ")
+code = 0
 print("loading prologue...")
 print("you are chilling by yourself when out of nowhere,\na small red envelope",
       "falls from the sky and lands right in front of you.")
 while True:
     yesno = input("open it? ")
     if yesno == "yes":
-        print('"Dear {},\nyou have been chosen for a very special competition!\nFind me at xxx to play. Theres a big sum of money at stake.\nSee you there!\n- Your host"'.format(name))
+        print('"Dear {},\nyou have been chosen for a very special competition!\nFind me at xxx to play. Theres a big sum of money at stake.\nSee you there!\n- Your host"\n'.format(name))
         print("excited for a chance at lots of money, you head off\nto this location with the envelope in hand.")
-        print("as you walk down the street, you unknowingly step on\na faded missing poster picturing a padlock and a scarf.")
+        print("as you walk down the street, you unknowingly step on\na faded missing poster featuring a padlock and a scarf.\n")
         break
     elif yesno == "no":
         print("no reason to do that, anyway.")
@@ -23,5 +23,8 @@ while True:
         break
     else:
         print("sorry, i didn't understand that.")
+lazy = str(lazy)
+code = lazy+","+name+","+object
+print("here is your chapter code. please keep it saved for the next chapter.")
+print(code)
 print("end of prologue. thank you for playing!")
-print("YOU SUCK AND ARE VERY DUMB!!!")
